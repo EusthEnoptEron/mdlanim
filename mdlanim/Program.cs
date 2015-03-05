d1c39a04b381facbf3f09da59d5a5d6f6f11fd10
Simon Meer <simon.meer@bluewin.ch>
2015-03-05 21:28:28 +0100

    Initial commit.

---
+++ mdlanim/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using BrawlLib.Modeling;
+using System.Linq;
+using BrawlLib.SSBB.ResourceNodes;
+using System.IO;
+
+
+namespace mdlanim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mdl0path = args.FirstOrDefault(f => f.EndsWith(".mdl0"));
+            var anims = args.Where(f => f.EndsWith(".chr0"));
+            var pac   = args.FirstOrDefault(f => f.EndsWith(".pac"));
+
+            if (mdl0path != null)
+            {
+                var outputPath = Path.GetFileNameWithoutExtension(mdl0path) + ".dae";
+                var model = MDL0Node.FromFile(mdl0path);
+                var animations = anims.Select(anim => CHR0Node.FromFile(anim));
+
+                // First write the DAE file
+                Collada.Serialize(model, outputPath);
+
+                // Write animations    
+                Collada.Serialize(model, animations.ToArray(), 60f, outputPath);
+            }
+            else if(pac != null) {
+                ((ARCNode)NodeFactory.FromFile(null, pac)).ExtractToFolder(Path.GetFileNameWithoutExtension(pac) + ".extracted");
+
+            } 
+            else
+            {
+                Console.Error.WriteLine("No mdl0 specified!");
+            }
+        }
+    }
+}
